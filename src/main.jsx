551e418509d0ebe2d9570c774280611e62e4c69d
saraiesco <saraiescobar@google.com>
2024-04-08 16:18:31 -0500

    :(

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,12 @@ import {
 import './index.css'
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +25,13 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   }, 
